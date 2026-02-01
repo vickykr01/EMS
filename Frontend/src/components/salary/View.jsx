59fbb673cf9403ad6f0bf81eb3536760e604b588
Vicky Kumar <145155514+vickykr01@users.noreply.github.com>
2026-02-02 00:45:35 +0530

    Update View.jsx

--- Frontend/src/components/salary/View.jsx
+++ Frontend/src/components/salary/View.jsx
@@ -11,7 +11,7 @@ const View = () => {
   const fetchSalaries = async () => {
     try {
       const response = await axios.get(
-        `http://localhost:3000/api/salary/${id}`,
+        `https://ems-server-i55t.onrender.com/api/salary/${id}`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
