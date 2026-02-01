42529d676642bb53a9c9e88b09c4fa30ff806077
Vicky Kumar <145155514+vickykr01@users.noreply.github.com>
2026-02-02 00:40:12 +0530

    Update DepartmentHelper.jsx

--- Frontend/src/utils/DepartmentHelper.jsx
+++ Frontend/src/utils/DepartmentHelper.jsx
@@ -27,7 +27,7 @@ export const DepartmentButtons = ({ Id, onDepartmentDelete }) => {
     if (confirm) {
       try {
         const response = await axios.delete(
-          `http://localhost:3000/api/department/${id}`,
+          `https://ems-server-i55t.onrender.com/api/department/${id}`,
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
